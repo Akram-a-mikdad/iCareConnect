e760008f31dcf1c7418af74ee0c1d5812fe269f0
Akram-a-mikdad <113662418+Akram-a-mikdad@users.noreply.github.com>
2023-12-29 20:16:27 +0300

    Update privileges-and-roles.service.ts

--- ui/src/app/shared/services/privileges-and-roles.service.ts
+++ ui/src/app/shared/services/privileges-and-roles.service.ts
@@ -18,60 +18,49 @@ export class PrivilegesAndRolesService {
   constructor(private api: Api) {}
 
   getPrivileges(parameters: any): Observable<PrivilegeGetFull[]> {
-    return from(this.api.privilege.getAllPrivileges(parameters)).pipe(
+    return this.api.privilege.getAllPrivileges(parameters).pipe(
       map((response) => response?.results),
       catchError((error) => of(error))
     );
   }
 
   deletePrivilege(id: string): Observable<any> {
-    return from(this.api.privilege.deletePrivilege(id)).pipe(
-      map((response) => response),
+    return this.api.privilege.deletePrivilege(id).pipe(
       catchError((error) => of(error))
     );
   }
 
   addNewPrivilege(privilege: PrivilegeCreate): Observable<PrivilegeCreateFull> {
-    return from(this.api.privilege.createPrivilege(privilege)).pipe(
-      map((response) => {
-        return response;
-      }),
+    return this.api.privilege.createPrivilege(privilege).pipe(
       catchError((error) => of(error))
     );
   }
 
   getRoles(parameters: any): Observable<RoleGetFull[]> {
-    return from(this.api.role.getAllRoles(parameters)).pipe(
+    return this.api.role.getAllRoles(parameters).pipe(
       map((response) => response?.results),
       catchError((error) => of(error))
     );
   }
 
   deleteRole(id: string): Observable<any> {
-    return from(this.api.role.deleteRole(id)).pipe(
-      map((response) => response),
+    return this.api.role.deleteRole(id).pipe(
       catchError((error) => of(error))
     );
   }
 
   addNewOrUpdateRole(role: RoleCreate): Observable<RoleCreateFull> {
-    return (
-      role?.uuid
-        ? from(this.api.role.updateRole(role?.uuid, role))
-        : from(this.api.role.createRole(role))
-    ).pipe(
-      map((response) => {
-        return response;
-      }),
+    const request = role?.uuid
+      ? this.api.role.updateRole(role?.uuid, role)
+      : this.api.role.createRole(role);
+
+    return from(request).pipe(
       catchError((error) => of(error))
     );
   }
 
   getRoleById(id: string): Observable<RoleGetFull> {
-    return from(this.api.role.getRole(id, { v: "full" })).pipe(
-      map((response) => {
-        return response;
-      }),
+    return this.api.role.getRole(id, { v: "full" }).pipe(
       catchError((error) => of(error))
     );
   }
